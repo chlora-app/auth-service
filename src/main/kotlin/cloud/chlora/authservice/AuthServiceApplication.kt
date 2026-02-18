93bd3b93050acca181419b102c1f980fa1e4d1eb
BleKuntay <riki.mcdougall@hotmail.com>
2026-02-19 03:35:39 +0700

    Initialize Spring Boot project with Maven wrapper, basic configuration, and dependencies.

---
+++ src/main/kotlin/cloud/chlora/authservice/AuthServiceApplication.kt
@@ -0,0 +1,13 @@
+package cloud.chlora.authservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+class AuthServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AuthServiceApplication>(*args)
+}
